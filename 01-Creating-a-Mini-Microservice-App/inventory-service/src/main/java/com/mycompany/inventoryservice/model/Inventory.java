8fdbecf54f0f7a36ba5364f21dbbc7613c61e3e2
serg10arg <cabrerasergio094@gmail.com>
2025-10-26 09:07:45 -0300

    feat(inventory-service): implementar endpoint de estado de inventario
    Añade el nuevo microservicio 'inventory-service' en el puerto 8003
    
    - Expone el endpoint GET /inventory/{productid} en 'InventoryController'
    - El controlador devuelve datos de inventario (en stock) desde una lista
      local hardcodeada.
    - Incluye el DTO 'Inventory'

---
+++ 01-Creating-a-Mini-Microservice-App/inventory-service/src/main/java/com/mycompany/inventoryservice/model/Inventory.java
@@ -0,0 +1,15 @@
+package com.mycompany.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+
+    private Long inventoryID;
+    private Long productID;
+    private Boolean inStock;
+}
